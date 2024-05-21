1c8b5c8100970e3813a72368f00992f84eb4a2f8
JoelCoyos <coyos52@gmail.com>
2024-05-21 17:26:59 -0300

    servidor esqueleto

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -3,3 +3,13 @@ const axios = require("axios")
 
 const app = express()
 const port = 8080;
+
+app.get('/datos', async (req, res) => {
+  //devolver los datos de la db
+});
+
+InsertarDatosRandom()
+{
+
+}
+
