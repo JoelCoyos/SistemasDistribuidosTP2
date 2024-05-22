895c957c0424064b30b0b04af52aaece934a7b30
Josefina Oller <64923768+JosefinaOller@users.noreply.github.com>
2024-05-22 10:15:23 -0300

    Integracion de conexion bd con interfaz
    Arroja error, no logra realizar conexión.

--- interfaz/interfaz.js
+++ interfaz/interfaz.js
@@ -1,13 +1,36 @@
 const express = require("express");
-//const axios = require("axios")
 
+//const db = nano.db.use("datos_aleatorios"); //usa la base de datos llamada datos_aleatorios, para probar aunque lo prolijo seria create
+
+const nano = require('nano')('http://localhost:5984'); //en internet figura que hay que poner http://admin:admin@localhost:5984
 const app = express()
 const port = 8080;
 
-//app.get("/datos",async (req,res)=>
-//{
-  //Agarrar los datos de la base de datos
-//});
+nano.db.list((err, body) => { //no iria pero es para ver si se realizo la conexion y arroja error
+  if (err) {
+    console.error("Error al listar las bases de datos:", err);
+  } else {
+    console.log("Bases de datos disponibles:", body);
+  }
+});
+
+function generarDatosAleatorios(){
+  return {
+    nombre: "Usuario" + Math.floor(Math.random()*1000),
+    edad: Math.floor(Math.random()*100),
+  };
+}
+
+app.get("/datos",async (req,res)=>
+{
+  const datos = generarDatosAleatorios();
+  try{
+    const result = await db.insert(datos);
+    res.status(200).json(result);
+  } catch (error){
+    res.status(500).json({ error: "Error al insertar datos en la base de datos"});
+  }
+});
 
 
 app.get("/", async (req, res) => {
