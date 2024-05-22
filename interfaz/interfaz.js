1ae25d74afbea88b05c7e3e833ebd73b3f36ab36
JoelCoyos <coyos52@gmail.com>
2024-05-22 14:05:47 -0300

    separado interfaz y servidor

--- interfaz/interfaz.js
+++ interfaz/interfaz.js
@@ -1,48 +1,6 @@
 const express = require("express");
-const nano = require('nano')('http://admin:admin@172.17.0.2:5984'); 
 const app = express()
 const port = 8080;
-
-// Crear la base de datos "datos_aleatorios"
-nano.db.create('datos_aleatorios', function(err, body) {
-  if (err) {
-    console.error("Error al crear la base de datos 'datos_aleatorios':", err);
-  } else {
-    console.log("Base de datos 'datos_aleatorios' creada correctamente");
-  }
-});
-
-function generarDatosAleatorios(){
-  return {
-    nombre: "Usuario" + Math.floor(Math.random()*1000),
-    edad: Math.floor(Math.random()*100),
-  };
-}
-
-
-
-app.get("/datos", (req, res) => {
-  // Crear la instancia de la base de datos 'db' utilizando nano.db.use
-  const db = nano.db.use('datos_aleatorios');
-
-  // Función para insertar datos en la base de datos
-  async function insertarDatos() {
-    try {
-      const datos = generarDatosAleatorios();
-      const result = await db.insert(datos);
-      console.log("Datos insertados correctamente:", result);
-    } catch (error) {
-      console.error("Error al insertar datos en la base de datos:", error);
-    }
-  }
-
-  // Insertar datos inicialmente y luego cada segundo
-  insertarDatos(); // Insertar datos inicialmente
-  setInterval(insertarDatos, 1000); // Insertar datos cada segundo
-
-  res.status(200).json({ message: "Generando datos constantemente" });
-});
-
 
 
 app.get("/", async (req, res) => {
--- servidor/servidor.js
+++ servidor/servidor.js
@@ -1,15 +1,47 @@
 const express = require("express");
-const axios = require("axios")
-
+const nano = require('nano')('http://admin:admin@172.17.0.2:5984'); 
+const db = nano.db.use('datos_aleatorios');
 const app = express()
-const port = 8080;
+const port = 8081;
 
-app.get('/datos', async (req, res) => {
-  //devolver los datos de la db
+// Crear la base de datos "datos_aleatorios"
+nano.db.create('datos_aleatorios', function(err, body) {
+  if (err) {
+    console.error("Error al crear la base de datos 'datos_aleatorios':", err);
+  } else {
+    console.log("Base de datos 'datos_aleatorios' creada correctamente");
+  }
 });
 
-InsertarDatosRandom()
-{
-
+function generarDatoAleatorios(){
+  return {
+    nombre: "Usuario" + Math.floor(Math.random()*1000),
+    edad: Math.floor(Math.random()*100),
+  };
 }
 
+app.get("/datos", async (req, res) => {
+  try
+  {
+    const datos = await db.list({include_docs:true});
+    res.json(datos);
+  }
+  catch(error)
+  {
+    res.status(500).json({error:error.message});
+  }
+});
+
+
+// Función para insertar datos en la base de datos
+async function insertarDatos() {
+  try {
+    const datos = generarDatoAleatorios();
+    const result = await db.insert(datos);
+    console.log("Datos insertados correctamente:", result);
+  } catch (error) {
+    console.error("Error al insertar datos en la base de datos:", error);
+  }
+}
+
+setInterval(insertarDatos, 1000); // Insertar datos cada segundo
